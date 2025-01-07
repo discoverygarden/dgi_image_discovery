b6d4e8cf9dad2a47afc320b682f666717cc167af
MorganDawe <mdawe@discoverygarden.ca>
2025-01-07 15:32:14 -0400

    Modifying field_default_image retrieval.

--- src/EventSubscriber/ModelDefaultFallbackSubscriber.php
+++ src/EventSubscriber/ModelDefaultFallbackSubscriber.php
@@ -26,39 +26,30 @@ class ModelDefaultFallbackSubscriber extends AbstractImageDiscoverySubscriber {
     /** @var \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem $term_item */
     foreach ($entity->get('field_model') as $term_item) {
       /** @var \Drupal\taxonomy\TermInterface $term */
-      dsm($term_item);
       $term = $term_item?->get('entity')?->getTarget()?->getEntity();
-      dsm($term);
       $term_access = $term?->access('view', NULL, TRUE);
-      dsm($term_access->isAllowed());
+
       if (!$term_access?->isAllowed()) {
-        dsm("a");
         continue;
       }
+
       if (!$term?->hasField('field_default_image')) {
-        dsm("b");
         continue;
       }
-      dsm("c");
-      $dt = $term->get('field_default_image');
-      dsm($dt);
+
       /** @var \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem $candidate_item */
       foreach ($term->get('field_default_image') as $candidate_item) {
-        dsm("d");
-        dsm($candidate_item);
-        $candidate = $candidate_item?->get('entity')?->getTarget()->getEntity();
-        dsm($candidate);
-//        /** @var \Drupal\media\MediaInterface $candidate */Drupal\file\Entity\File
-//        /** @var \Drupal\file\Entity\File $candidate */
-//        if ($candidate = $candidate_item?->get('entity')?->getTarget()->getEntity()) {
-//          continue;
-//        }
-        dsm("e");
+
+        /** @var \Drupal\media\Entity\Media $candidate */
+        if (!$candidate = $candidate_item?->get('entity')?->getTarget()?->getEntity()) {
+          continue;
+        }
+
         $candidate_access = $candidate?->access('view', NULL, TRUE);
-//        if (!$candidate_access->isAllowed()) {
-//          continue;
-//        }
-        dsm("f");
+        if (!$candidate_access->isAllowed()) {
+          continue;
+        }
+
         $event->addCacheableDependency($term_access)
           ->addCacheableDependency($candidate_access)
           ->addCacheableDependency($term)
